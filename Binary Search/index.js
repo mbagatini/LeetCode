f87274375798d016d74766710d39aa443008444d
Morgana <morganabagatini@gmail.com>
2022-08-04 00:18:55 -0300

    binary search

--- Binary Search/index.js
+++ Binary Search/index.js
@@ -6,26 +6,25 @@
 var search = function (nums, target) {
 	var ini = 0;
 	var end = nums.length - 1;
-	var mid = Math.round((ini + end) / 2);
+	var mid = parseInt((ini + end) / 2);
 
 	//  0 1  2 3 4 5 
-	// -1,0, 3,5,9,12
+	// -1,0, 3,5,9,12    
 
-	while (ini != end) {
+	while (ini <= end) {
 		if (nums[mid] == target) {
 			return mid;
-		}
-
-		if (nums[mid] > target) {
+		} else if (nums[mid] > target) {
 			end = mid - 1;
 		} else {
 			ini = mid + 1;
 		}
 
-		mid = Math.round((ini + end) / 2);
+		mid = parseInt((ini + end) / 2);
 	}
 
 	return -1;
 };
 
+search([5], 5);
 search([-1, 0, 3, 5, 9, 12], 2);
